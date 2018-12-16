d05726ac271e14b423edff2210aa4b0bdf14352d
Pavol Sovis <sovis@vectary.com>
2018-12-16 14:27:48 +0100

    copy plugin for webpack, with a very naive example for img

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: ['babel-polyfill', './src/js/index.js'],
@@ -14,7 +15,19 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
+        new CopyWebpackPlugin([
+            // npm i -D copy-webpack-plugin
+            {
+                from: "./src/img/*.*",
+                to: "./img",
+                transformPath (targePath, absolutePath) {
+                    // console.log(targePath, absolutePath);
+                    const file = absolutePath.split("/").pop();
+                    return Promise.resolve(`./img/${file}`)
+                }
+            }
+        ])
     ],
     module: {
         rules: [
