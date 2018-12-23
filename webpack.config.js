7de7f05cac388af380f985c9193aef75a2b9a333
Martin Bilik <martinbilik@hotmail.sk>
2018-12-23 07:36:22 +0100

    copy plugin for webpack, with very naive example for css

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new CopyWebpackPlugin([
+            // npm i -D copy-webpack-plugin
+            {
+                from: "./src/img/*.*",
+                to: "./img",
+                transformPath (targePath, absolutePath) {
+                    // console.log(targePath, absolutePath);
+                    const file = absolutePath.split("/").pop();
+                    return Promise.resolve(`./img/${file}`)
+                }
+            },
+            {
+                from: "./src/css/*.*",
+                to: "./css",
+                transformPath (targePath, absolutePath) {
+                    // console.log(targePath, absolutePath);
+                    const file = absolutePath.split("/").pop();
+                    return Promise.resolve(`./css/${file}`)
+                } 
+            }
+        ])
+    ],
+    module: {
+        rules: [
+            {
+                test: /.\js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
