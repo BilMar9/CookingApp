8ae47018207e812766c84611d3e16ab79aa2d6f4
Martin Bilik <martinbilik@hotmial.sk>
2019-01-30 15:01:43 +0100

    Header, Left Panel

--- src/js/views/components/leftPanel.ts
+++ src/js/views/components/leftPanel.ts
@@ -1,16 +1,16 @@
 import { Widget, element } from "./widget";
-import { ResultsForm } from "./resultsForm";
+import { Results } from "./results";
 
 export class LeftPanel implements Widget {
 
     private el: HTMLElement;
-    private resultsForm: ResultsForm;
+    private results: Results;
 
     element(): HTMLElement {
         if (!this.el) {
             this.el = element(`<div class="results">`);
-            this.resultsForm = new ResultsForm();
-            this.el.appendChild(this.resultsForm.element());
+            this.results = new Results();
+            this.el.appendChild(this.results.element());
         }
         return this.el;
     }
